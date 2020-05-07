ba7b0d0330406a2c5f7d3e73b5aa9adc13ed6c3d
Armen96 <barsegyan96armen@gmail.com>
2020-05-07 22:11:26 +0400

    [Daily Problem] Smallest Number not a Sum of Subset of List

---
+++ daily interview pro/smallest-number-that-is-not-a-sum-of-a-subset-of-list.js
@@ -0,0 +1,13 @@
+/**
+ * Hi, here's your problem today. This problem was recently asked by AirBNB:
+ * Given a sorted list of positive numbers, find the smallest positive number that cannot be a sum of any subset in the list.
+ *
+ * Input: [1, 2, 3, 8, 9, 10]
+ * Output: 7
+ */
+
+function findSmallest(array) {
+
+}
+
+console.log(findSmallest([1, 2, 3, 8, 9, 10]));
