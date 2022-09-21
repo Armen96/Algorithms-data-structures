e8ea4abab42817f4c12e3ebe2eca2354e4f9f7c0
Armen96 <barsegyan96armen@gmail.com>
2022-09-21 22:34:34 +0400

    Sort Colors v3

--- leetcode/75. Sort Colors.js
+++ leetcode/75. Sort Colors.js
@@ -33,3 +33,21 @@ var sortColors = function(nums) {
 console.time();
 console.log(sortColors([2,0,2,1,1,0,0,1]));
 console.timeEnd();
+
+
+var sortColorsV2 = function(nums) {
+    let checkCounts = [0, 0, 0];
+    for (let i = 0; i < nums.length; i++) {
+        checkCounts[nums[i]] = checkCounts[nums[i]] + 1;
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+        nums[i] = i < checkCounts[0] ? 0 : i < checkCounts[0] + checkCounts[1] ? 1 : 2;
+    }
+
+    return nums;
+};
+
+console.time();
+console.log(sortColorsV2([2,0,2,1,1,0,0,1]));
+console.timeEnd();
