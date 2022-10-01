b6938540d10397954fc2927d08bba20f3395983a
Armen96 <barsegyan96armen@gmail.com>
2022-10-01 23:06:14 +0400

    Longest Palindrome

--- leetcode/easy/409. Longest Palindrome.js
+++ leetcode/easy/409. Longest Palindrome.js
@@ -3,7 +3,25 @@
  */
 
 let longestPalindrome = function(s) {
+    let memo = {};
+    let count = 0;
 
+    for (let i = 0; i < s.length; i++) {
+        memo[s[i]] = memo[s[i]] ? memo[s[i]] + 1 : 1;
+    }
+
+    let keys = Object.keys(memo);
+    let isOdd = false;
+    keys.forEach(key => {
+        if (memo[key] %2 === 0) {
+            count = count + memo[key];
+        } else {
+            isOdd = true;
+            count = count + memo[key] - 1;
+        }
+    })
+
+    return isOdd ? count + 1 : count;
 };
 
 console.log(longestPalindrome('abccccdd'));
